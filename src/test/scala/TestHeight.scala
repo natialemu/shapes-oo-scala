d0d0b3838a3720f43cb7a4b67923b98f47932cff
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-02-25 20:13:44 -0600

    testing changes

--- src/test/scala/TestHeight.scala
+++ src/test/scala/TestHeight.scala
@@ -1,6 +1,29 @@
+import edu.luc.cs.laufer.cs372.shapes.Shape
+import edu.luc.cs.laufer.cs372.shapes.TestFixtures._
+import edu.luc.cs.laufer.cs372.shapes.behaviors._
+import org.scalatest.FunSuite
+
+
 /**
   * Created by Nathnael on 2/25/2017.
   */
-class TestHeight {
+class TestHeight extends FunSuite{
+  def testHeight(description: String, s: Shape, sz : Int) = {
+    test(description) {
+
+
+      //val r = b.shape.asInstanceOf[Rectangle]
+      assert(height(s) == sz)
+    }
+  }
+
+  // TODO comment these tests back in
+
+  testHeight("simple ellipse", simpleEllipse, 1)
+  testHeight("simple rectangle", simpleRectangle, 1)
+  testHeight("simple location", simpleLocation, 1)
+  testHeight("basic group", basicGroup, 2)
+  testHeight("simple group", simpleGroup, 2)
+  testHeight("complex group", complexGroup, 3)
 
 }
--- src/test/scala/TestScale.scala
+++ src/test/scala/TestScale.scala
@@ -1,6 +1,42 @@
+import edu.luc.cs.laufer.cs372.shapes.{Ellipse, Rectangle, Shape}
+import edu.luc.cs.laufer.cs372.shapes.TestFixtures._
+import edu.luc.cs.laufer.cs372.shapes.behaviors._
+import org.scalatest.FunSuite
+
 /**
   * Created by Nathnael on 2/25/2017.
   */
-class TestScale {
+class TestScale extends FunSuite{
+
+    test("simple ellipse rescale test") {
+      val newShape = rescale(simpleEllipse,2)
+      assert(newShape === Ellipse(simpleEllipse.majorAxis*2,simpleEllipse.minorAxis*2))
+    }
+
+  test("simple rectangle rescale test") {
+    val newShape = rescale(simpleRectangle,2)
+    assert(newShape === Rectangle(simpleRectangle.width*2,simpleRectangle.height*2))
+  }
+
+  test("simple location rescale test") {
+    val newShape = rescale(simpleLocation,2)
+    assert(newShape === Ellipse(simpleEllipse.majorAxis*2,simpleEllipse.minorAxis*2))
+  }
+
+  test("basic group rescaling test") {
+    val newShape = rescale(basicGroup,2)
+    assert(newShape === Ellipse(simpleEllipse.majorAxis*2,simpleEllipse.minorAxis*2))
+  }
+
+  test("simple group rescaling test") {
+    val newShape = rescale(simpleGroup,2)
+    assert(newShape === Ellipse(simpleEllipse.majorAxis*2,simpleEllipse.minorAxis*2))
+  }
+  test("complex group rescaling test") {
+    val newShape = rescale(complexGroup,2)
+    assert(newShape === Ellipse(simpleEllipse.majorAxis*2,simpleEllipse.minorAxis*2))
+  }
+
+
 
 }
